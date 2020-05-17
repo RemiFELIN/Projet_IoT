6ea14870eee3b5cc67ebae5a30ee185efdd3ba08
loubard <yannick.cardini@gmx.com>
2020-05-17 16:58:41 +0200

    :boom:

--- iot-frontend/src/app/app.module.ts
+++ iot-frontend/src/app/app.module.ts
@@ -4,16 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CapteursComponent } from './capteurs/capteurs.component';
-import { GraphSonComponent } from './graph-son/graph-son.component';
-import { GraphMouvementComponent } from './graph-mouvement/graph-mouvement.component';
 import { GraphGazComponent } from './graph-gaz/graph-gaz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CapteursComponent,
-    GraphSonComponent,
-    GraphMouvementComponent,
     GraphGazComponent,
   ],
   imports: [
--- iot-frontend/src/app/capteurs/capteurs.component.ts
+++ iot-frontend/src/app/capteurs/capteurs.component.ts
@@ -1,21 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GetCapteursService } from '../services/get-capteurs.service';
+import { Subscription } from 'rxjs';
+import { Capteur } from '../capteurs';
+import { capteurs } from '../mock-capteurs';
+import {FormControl, FormGroup} from '@angular/forms';
 @Component({
   selector: 'app-capteurs',
   templateUrl: './capteurs.component.html',
   styleUrls: ['./capteurs.component.css']
-
 })
+
 export class CapteursComponent implements OnInit {
 
   gaz: number = 0;
   mouvement: number = 0;
   bruit: number = 1;
+  subscription: Subscription;
+  listOfCapteur: Array<Capteur> = new Array<Capteur>();
+  selectedValue: string = "micro";
+  ip:string;
+  capteur: Capteur;
+  capteurs: Capteur[];
+  data: Capteur;
+  states = [
+    {name: 'Arizona', abbrev: 'AZ'},
+    {name: 'California', abbrev: 'CA'},
+    {name: 'Colorado', abbrev: 'CO'},
+    {name: 'New York', abbrev: 'NY'},
+    {name: 'Pennsylvania', abbrev: 'PA'},
+  ];
 
-  constructor() { }
+  form = new FormGroup({
+    state: new FormControl(this.states[3])})
+
+  constructor(private capteurService: GetCapteursService) { 
+    this.capteurs = capteurs;
+    this.subscription = this.capteurService.getCapteurs()
+    .subscribe(capteurs => this.data = capteurs)
+  }
+
+  addCapteur(){
+    console.log(capteurs)
+    const c = capteurs.find(c => console.log(this.selectedValue));
+    console.log(c);
+    // this.listOfCapteur.push(c);
+  }
 
   ngOnInit(): void {
   }
-   
 
 }

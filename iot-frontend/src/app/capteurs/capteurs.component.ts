c4264f6bb32d695da23ff91defd09eca0fbb12e5
loubard <yannick.cardini@gmx.com>
2020-05-17 22:03:53 +0200

    update value :bouquet:

--- iot-frontend/src/app/capteurs/capteurs.component.ts
+++ iot-frontend/src/app/capteurs/capteurs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { GetCapteursService } from '../services/get-capteurs.service';
 import { Subscription } from 'rxjs';
 import { Capteur } from '../capteurs';
@@ -23,28 +23,36 @@ export class CapteursComponent {
   ip: string;
   capteur: Capteur;
   capteurs: Capteur[];
+  @Input() radioButton: boolean;
 
 
-  constructor(private capteurService: GetCapteursService,private modalService: NgbModal) {
+  constructor(private capteurService: GetCapteursService, private modalService: NgbModal) {
     this.capteurs = capteurs;
     this.subscription = this.capteurService.getCapteurs()
       .subscribe(cap => {
-        capteurs.forEach(c => {
-          cap.forEach(cs => {
-            if (c.type == cs.type && this.isNotInListOfCapteur(c)) {
-              const cap = c;
-              cap.who = cs.who;
-              cap.value = cs.value;
-              this.listOfCapteur.push(cap);
-            }
-          })
-        })
+        this.listOfCapteur = this.addProperties(cap);
       });
     this.capteurService.requestCapteurs();
   }
 
-  openModal(content,capt : Capteur) {
-    this.modalService.open(content, {size:'xl'});
+  addProperties(arr: Capteur[]): Capteur[] {
+    var res = [];
+    arr.forEach(ca => {
+      capteurs.forEach(cap => {
+        if(ca.type == cap.type){
+          var tmp = cap;
+          tmp.who = ca.who;
+          tmp.value =  ca.value;
+          console.log(ca.value)
+          res.push(tmp)
+        }
+      })
+    })
+    return res;
+  }
+
+  openModal(content, capt: Capteur) {
+    this.modalService.open(content, { size: 'xl' });
     var ctx = document.getElementById("graph");
     var myChart = new Chart(ctx, {
       type: 'line',
@@ -52,7 +60,7 @@ export class CapteursComponent {
         labels: ['-60s', '-50s', '-40s', '-30s', '-20s', '-10s', 'Maintenant'],
         datasets: [{
           label: capt.type,
-          data: capt.value ? capt.value : [1,2,2,2,1,2,1],
+          data: capt.value ,
           backgroundColor: [
             'rgba(147,112,219,0.2)',
           ],
@@ -86,19 +94,20 @@ export class CapteursComponent {
     return res;
   }
 
- async addCapteur() {
+  async addCapteur() {
     const capteurExist = await this.capteurService.checkIfExist(this.ip);
-    if(capteurExist){
+    if (capteurExist) {
       capteurs.forEach(c => {
         if (c.type == this.selectedValue.type) {
           const cap = c;
           cap.who = this.ip;
+          this.capteurService.ajouter(c.who);
           this.listOfCapteur.push(c);
         }
       })
       this.capteurService.updateCapteurs(this.listOfCapteur);
     }
-    else{
+    else {
       alert("Aucun object connecté avec cette adresse IP");
     }
 
